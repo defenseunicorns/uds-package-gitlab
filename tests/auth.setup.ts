2c34cf96803e6bc921574f7024f361dc4b3a3690
Marshall Cottrell <screwyproduct@gmail.com>
2024-05-24 16:45:03 -0400

    chore: add basic smoke tests with sso, git commit, and file uploads (#132)
    Fixes #130

---
+++ tests/auth.setup.ts
@@ -0,0 +1,26 @@
+import { test as setup, expect } from '@playwright/test';
+import { authFile } from './playwright.config';
+
+setup('authenticate', async ({ page, context }) => {
+  await page.goto('/');
+
+  await page.getByLabel('Username or email').fill('doug');
+  await page.getByLabel('Password').fill('unicorn123!@#');
+  await page.getByRole('button', { name: "Log In" }).click();
+
+  await page.waitForURL('/');  // successful redirect
+
+  // ensure auth cookies were set
+  const cookies = await context.cookies();
+  const keycloakCookie = cookies.find(
+    (cookie) => cookie.name === "KEYCLOAK_SESSION",
+  );
+
+  expect(keycloakCookie).toBeDefined();
+  expect(keycloakCookie?.value).not.toBe("");
+  expect(keycloakCookie?.domain).toContain("sso.");
+
+  await page.context().storageState({ path: authFile });
+
+  await expect(page).toHaveURL('/');
+})
